05e8b7da7b4f7fce7a54b5b236c79af7df97f32f
Andreas Amstutz <tullo@users.noreply.github.com>
2023-03-20 20:17:10 +0100

    add missing test function

--- Chapter02/stores/stores_test.go
+++ Chapter02/stores/stores_test.go
@@ -2,6 +2,7 @@ package stores
 
 import (
 	"fmt"
+	"testing"
 
 	"github.com/cucumber/godog"
 )
@@ -36,4 +37,19 @@ func InitializeScenario(ctx *godog.ScenarioContext) {
 	ctx.Step(`^a valid store owner$`, aValidStoreOwner)
 	ctx.Step(`^I create the store called "([^"]*)"$`, iCreateTheStoreCalled)
 	ctx.Step(`^no store called "([^"]*)" exists$`, noStoreCalledExists)
+}
+
+func TestFeatures(t *testing.T) {
+	suite := godog.TestSuite{
+		ScenarioInitializer: InitializeScenario,
+		Options: &godog.Options{
+			Format:   "pretty",
+			Paths:    []string{"features"},
+			TestingT: t, // Testing instance that will run subtests.
+		},
+	}
+
+	if suite.Run() != 0 {
+		t.Fatal("non-zero status returned, failed to run feature tests")
+	}
 }
