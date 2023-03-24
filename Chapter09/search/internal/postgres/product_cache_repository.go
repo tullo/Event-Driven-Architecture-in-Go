99210c809a21198387d32b6a558b12846228106a
Andreas Amstutz <tullo@users.noreply.github.com>
2023-03-24 16:50:05 +0100

    fix product scanning

--- Chapter09/search/internal/postgres/product_cache_repository.go
+++ Chapter09/search/internal/postgres/product_cache_repository.go
@@ -63,13 +63,13 @@ func (r ProductCacheRepository) Remove(ctx context.Context, productID string) er
 }
 
 func (r ProductCacheRepository) Find(ctx context.Context, productID string) (*models.Product, error) {
-	const query = `SELECT store_id, name, price FROM %s WHERE id = $1 LIMIT 1`
+	const query = `SELECT store_id, name FROM %s WHERE id = $1 LIMIT 1`
 
 	product := &models.Product{
 		ID: productID,
 	}
 
-	err := r.db.QueryRowContext(ctx, r.table(query), productID).Scan(&product.Name)
+	err := r.db.QueryRowContext(ctx, r.table(query), productID).Scan(&product.StoreID, &product.Name)
 	if err != nil {
 		if !errors.Is(err, sql.ErrNoRows) {
 			return nil, errors.Wrap(err, "scanning product")
