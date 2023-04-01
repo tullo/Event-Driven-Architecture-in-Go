8a69ad045804abb27e0ae15b3a97d92254421154
Andreas Amstutz <tullo@users.noreply.github.com>
2023-04-01 11:54:20 +0200

    use amazon/aws-cli:2.11.8

--- Chapter11/deployment/setup-tools/set-aws-alias.sh
+++ Chapter11/deployment/setup-tools/set-aws-alias.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-alias aws='docker run --rm -ti -v ~/.aws:/root/.aws -v $(pwd):/aws -v ~/.kube:/root/.kube amazon/aws-cli'
+alias aws='docker run --rm -ti -v ~/.aws:/root/.aws -v $(pwd):/aws -v ~/.kube:/root/.kube amazon/aws-cli:2.11.8'
 
 echo "---"
 echo
